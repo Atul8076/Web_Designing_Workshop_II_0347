987a1cab00cb672bb3650add76bbfc2449ca43a7
atul yadav <atulyadav@atuls-MacBook-Air.local>
2026-03-19 09:41:13 +0530

    some change

---
+++ javascript/js_fundamental/m.js
@@ -0,0 +1,51 @@
+//let, var, const
+// var name = "Atul yadav";
+// var age = 30;
+// var isEmployed = true;
+// console.log(name);
+// console.log(age);
+// console.log(isEmployed);
+// var age=50;
+// console.log(age);
+
+// let name = "Atul yadav";
+// let age = 40;
+// let isEmployed = true;
+// console.log(name);
+// console.log(age);
+// console.log(isEmployed);
+// let age=50;
+// console.log(age);
+
+// 
+// if(true){
+//     var age =30;
+// }
+//  console.log(age);
+
+// if(true){
+//     let age =30;
+// }
+//  console.log(age);
+
+// const PI= 3.14;
+// console.log(PI);
+
+// PI=3.144
+// console.log(PI);
+
+// console.log(typeof name);
+
+// let name = "atul yadav";
+// let age1 = 20;
+// console.log("my name is " + name + " my age is " + age1);
+// console.log()
+
+let data=null;
+console.log(data);
+console.log(typeof data);
+
+
+let result;
+console.log(result);
+console.log(typeof result);
