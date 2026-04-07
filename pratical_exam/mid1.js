40acc327392e0d8cc7cc86b408ee2e1e0fb14d29
atul yadav <atulyadav@atuls-MacBook-Air.local>
2026-04-07 15:25:29 +0530

    pratical_exam

---
+++ pratical_exam/mid1.js
@@ -0,0 +1,12 @@
+let num1 = Number(prompt("Enter first number:"));
+let num2 = Number(prompt("Enter second number:"));
+
+
+let sum = num1 + num2;
+let difference = num1 - num2;
+let product = num1 * num2;
+
+
+console.log("Sum:", sum);
+console.log("Difference:", difference);
+console.log("Product:", product);
